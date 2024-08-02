63a9b627d4cb91c9e46083cd55e80e7854c49e83
Alexandre Neves <alxfn86@gmail.com>
2024-08-02 17:04:50 -0300

    implementing the product's listing route

--- eCommerce/src/main/java/com/alexandre/eCommerce/Controller/ProductController.java
+++ eCommerce/src/main/java/com/alexandre/eCommerce/Controller/ProductController.java
@@ -1,11 +1,30 @@
 package com.alexandre.eCommerce.Controller;
 
+import com.alexandre.eCommerce.DTO.ProductDTO;
+import com.alexandre.eCommerce.Domain.Product;
+import com.alexandre.eCommerce.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("products")
 public class ProductController {
+    private final ProductService service;
+
+    @Autowired
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<ProductDTO> products = service.findAll();
+        return ResponseEntity.ok(products);
+    }
 }
