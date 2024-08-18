ec2c49eea93775f98234c1855a91f299846ab3c1
Alexandre Neves <alxfn86@gmail.com>
2024-08-18 12:53:13 -0300

    docs: documentating OrderController

--- eCommerce/src/main/java/com/alexandre/eCommerce/Controller/order/OrderController.java
+++ eCommerce/src/main/java/com/alexandre/eCommerce/Controller/order/OrderController.java
@@ -6,6 +6,9 @@ import com.alexandre.eCommerce.Domain.user.UserAddress;
 import com.alexandre.eCommerce.services.TokenUtil;
 import com.alexandre.eCommerce.services.order.OrderService;
 import com.alexandre.eCommerce.services.user.AddressService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +20,12 @@ public class OrderController {
     TokenUtil tokenUtil;
     AddressService addressService;
 
+    @Operation(description = "Operation to create an order with products inside the cart.", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Order created successfully."),
+            @ApiResponse(responseCode = "400", description = "Operation cannot continue because the provided data is invalid."),
+            @ApiResponse(responseCode = "401", description = "Not authenticated.")
+    })
     @PostMapping
     public ResponseEntity<Order> proceedWithTheOrder(@RequestHeader String tokenHeader, @RequestBody CreateOrderRequest data){
         var userId = tokenUtil.extractIdFromToken(tokenHeader);
